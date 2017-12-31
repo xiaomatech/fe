bebfca2910d60ed44c8aa24b3e0c753d70a1ba22
xianhuawei <258022429@qq.com>
2017-12-31 20:42:14 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=7.6.0
+version=8.9.3
 
 wget https://nodejs.org/dist/v$version/node-v$version-linux-x64.tar.xz -O /tmp/node-v$version-linux-x64.tar.xz
 xz -d /tmp/node-v$version-linux-x64.tar.xz
@@ -12,4 +12,4 @@ echo 'export NODE_HOME=/opt/node
 export PATH=$PATH:$NODE_HOME/bin
 export NODE_PATH=$NODE_HOME/lib/node_modules'>>/etc/profile
 
-npm install pm2 vue vuex vue-cli vue-loader vue-router element-ui vue-datasource weui vux vue-echarts vue-amap weex-toolkit promise-polyfill -g
+npm install pm2 vue vuex vue-cli vue-loader vue-router vux vue-echarts element-ui vue-datasource weui wepy wepy wepy-cli zanui-weapp -g
